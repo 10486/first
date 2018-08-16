794823dbc261f810d320e9f9c0969539c8843148
Vlad Mezhevov <tsyduev.b.1@gmail.com>
2018-08-16 13:37:38 +0000

    changes into seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this->call(Admin::class);
-      $this->call(AdminPages::class);
+      $pages = [
+        'Главная' => 'admin',
+        'Все Записи' => 'adminPosts',
+        'Добавить Запись' => 'add',
+        'Регистрация' => 'registration',
+        'Выход' => 'logout'
+      ];
+      foreach ($pages as $name => $route) {
+        App\Admin::make([
+          'name' => $name,
+          'routeName' => $route
+        ])->save();
+      }
 
-      
+      App\User::make([
+        'name'=>env('ADMIN_NAME'),
+        'email'=>env('ADMIN_EMAIL'),
+        'password'=>bcrypt(env('ADMIN_PASSWORD','secret'))
+      ])->save();
+
       $pages = [
         'Главная' => 'home',
         'Все Записи' => 'posts',
